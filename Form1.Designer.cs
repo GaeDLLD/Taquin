af8ac24ca2cc85d6429b579b13ab967de53089ab
Gaëtan DELALANDE <gaetan.delalande@campus-igs-toulouse.fr>
2019-12-17 07:58:41 +0000

    Shuffled grid done

---
+++ Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace Taquin
+{
+    partial class Taquin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Taquin));
+            this.label_moves_made = new System.Windows.Forms.Label();
+            this.moves_made = new System.Windows.Forms.Label();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Label();
+            this.PuzzleBox = new System.Windows.Forms.GroupBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.suffle_btn = new System.Windows.Forms.Button();
+            this.pause_btn = new System.Windows.Forms.Button();
+            this.quitte_btn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.PuzzleBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label_moves_made
+            // 
+            this.label_moves_made.AutoSize = true;
+            this.label_moves_made.Location = new System.Drawing.Point(13, 828);
+            this.label_moves_made.Name = "label_moves_made";
+            this.label_moves_made.Size = new System.Drawing.Size(148, 25);
+            this.label_moves_made.TabIndex = 1;
+            this.label_moves_made.Text = "Moves Made :";
+            // 
+            // moves_made
+            // 
+            this.moves_made.AutoSize = true;
+            this.moves_made.Location = new System.Drawing.Point(167, 828);
+            this.moves_made.Name = "moves_made";
+            this.moves_made.Size = new System.Drawing.Size(36, 25);
+            this.moves_made.TabIndex = 2;
+            this.moves_made.Text = "00";
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox.Image")));
+            this.pictureBox.InitialImage = null;
+            this.pictureBox.Location = new System.Drawing.Point(912, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(600, 600);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 3;
+            this.pictureBox.TabStop = false;
+            // 
+            // timer
+            // 
+            this.timer.AutoSize = true;
+            this.timer.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timer.Location = new System.Drawing.Point(918, 626);
+            this.timer.Name = "timer";
+            this.timer.Size = new System.Drawing.Size(589, 153);
+            this.timer.TabIndex = 4;
+            this.timer.Text = "00:00:00";
+            // 
+            // PuzzleBox
+            // 
+            this.PuzzleBox.Controls.Add(this.pictureBox6);
+            this.PuzzleBox.Controls.Add(this.pictureBox9);
+            this.PuzzleBox.Controls.Add(this.pictureBox8);
+            this.PuzzleBox.Controls.Add(this.pictureBox7);
+            this.PuzzleBox.Controls.Add(this.pictureBox5);
+            this.PuzzleBox.Controls.Add(this.pictureBox4);
+            this.PuzzleBox.Controls.Add(this.pictureBox2);
+            this.PuzzleBox.Controls.Add(this.pictureBox3);
+            this.PuzzleBox.Controls.Add(this.pictureBox1);
+            this.PuzzleBox.Location = new System.Drawing.Point(12, 12);
+            this.PuzzleBox.Name = "PuzzleBox";
+            this.PuzzleBox.Size = new System.Drawing.Size(800, 800);
+            this.PuzzleBox.TabIndex = 0;
+            this.PuzzleBox.TabStop = false;
+            this.PuzzleBox.Text = "PuzzleBox";
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(529, 286);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(250, 250);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 8;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Location = new System.Drawing.Point(529, 542);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(250, 250);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox9.TabIndex = 7;
+            this.pictureBox9.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Location = new System.Drawing.Point(273, 542);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(250, 250);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 6;
+            this.pictureBox8.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Location = new System.Drawing.Point(17, 542);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(250, 250);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 5;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(273, 286);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(250, 250);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(17, 286);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(250, 250);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(273, 30);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(250, 250);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(529, 30);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(250, 250);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 1;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(17, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(250, 250);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // suffle_btn
+            // 
+            this.suffle_btn.Location = new System.Drawing.Point(912, 791);
+            this.suffle_btn.Name = "suffle_btn";
+            this.suffle_btn.Size = new System.Drawing.Size(134, 62);
+            this.suffle_btn.TabIndex = 5;
+            this.suffle_btn.Text = "Shuffle";
+            this.suffle_btn.UseVisualStyleBackColor = true;
+            this.suffle_btn.Click += new System.EventHandler(this.suffle_btn_Click);
+            // 
+            // pause_btn
+            // 
+            this.pause_btn.Location = new System.Drawing.Point(1142, 791);
+            this.pause_btn.Name = "pause_btn";
+            this.pause_btn.Size = new System.Drawing.Size(134, 62);
+            this.pause_btn.TabIndex = 6;
+            this.pause_btn.Text = "Pause";
+            this.pause_btn.UseVisualStyleBackColor = true;
+            // 
+            // quitte_btn
+            // 
+            this.quitte_btn.Location = new System.Drawing.Point(1372, 791);
+            this.quitte_btn.Name = "quitte_btn";
+            this.quitte_btn.Size = new System.Drawing.Size(134, 62);
+            this.quitte_btn.TabIndex = 7;
+            this.quitte_btn.Text = "button3";
+            this.quitte_btn.UseVisualStyleBackColor = true;
+            // 
+            // Taquin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1610, 865);
+            this.Controls.Add(this.quitte_btn);
+            this.Controls.Add(this.pause_btn);
+            this.Controls.Add(this.suffle_btn);
+            this.Controls.Add(this.timer);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.moves_made);
+            this.Controls.Add(this.label_moves_made);
+            this.Controls.Add(this.PuzzleBox);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1636, 936);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(1636, 936);
+            this.Name = "Taquin";
+            this.Text = "Taquin";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.PuzzleBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label_moves_made;
+        private System.Windows.Forms.Label moves_made;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label timer;
+        private System.Windows.Forms.GroupBox PuzzleBox;
+        private System.Windows.Forms.Button suffle_btn;
+        private System.Windows.Forms.Button pause_btn;
+        private System.Windows.Forms.Button quitte_btn;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Taquin
+{
+    public partial class Taquin : Form
+    {
+
+        private int[] the_list = {
+             1, 2, 3,
+             4, 5, 6,
+             7, 8, 0 
+        };
+
+        private List<PictureBox> all_picturebox = new List<PictureBox>();
+        private List<Image> all_image = new List<Image>();
+
+        public Taquin()
+        {
+            InitializeComponent();
+
+            all_picturebox.Add(pictureBox1);
+            all_picturebox.Add(pictureBox2);
+            all_picturebox.Add(pictureBox3);
+            all_picturebox.Add(pictureBox4);
+            all_picturebox.Add(pictureBox5);
+            all_picturebox.Add(pictureBox6);
+            all_picturebox.Add(pictureBox7);
+            all_picturebox.Add(pictureBox8);
+            all_picturebox.Add(pictureBox9);
+
+            all_image.Add(Properties.Resources._null);
+            all_image.Add(Properties.Resources._1);
+            all_image.Add(Properties.Resources._2);
+            all_image.Add(Properties.Resources._3);
+            all_image.Add(Properties.Resources._4);
+            all_image.Add(Properties.Resources._5);
+            all_image.Add(Properties.Resources._6);
+            all_image.Add(Properties.Resources._7);
+            all_image.Add(Properties.Resources._8);
+
+            refresh_display();
+
+            foreach (PictureBox pictureBox in all_picturebox)
+            {
+                
+
+            }
+        }
+
+        private void Taquin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void refresh_display()
+        {
+            for (int i = 0; i < the_list.Length; i++)
+            {
+                all_picturebox[i].Image = all_image[the_list[i]];
+            }
+            the_list.ToList().ForEach(i => Console.WriteLine(i.ToString()));
+        }
+
+        private void suffle_btn_Click(object sender, EventArgs e)
+        {
+            Random rng = new Random();
+            int n = the_list.Length;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                int value = the_list[k];
+                the_list[k] = the_list[n];
+                the_list[n] = value;
+            }
+            refresh_display();
+        }
+
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
